07d6401c9edcefe2c739c112607761bfc54385ef
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-01-25 10:23:28 -0500

    Début refonte tables, .notifier. Suite des motifs .actions.

--- extensions/snake/assets/custom.js
+++ extensions/snake/assets/custom.js
@@ -2,10 +2,17 @@
 	'use strict';
 	
 	var ready = function () {
+
+		/* Header - Nav */
+
 		$('#nav li').has('ul').on('click', function(){
 			$(this).toggleClass('opened');
 			$(this).children('ul').slideToggle(250);
 		});
+
+		/* Header - Actions */
+
+		if($('.page-single #contents .actions, .single #contents .actions').length) $('#context').addClass('spaced-right');
 	};
 	
 	$(ready);
