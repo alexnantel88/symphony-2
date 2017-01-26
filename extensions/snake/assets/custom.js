8db530b5035394d8a627db1a6524de03c21fd1ae
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-01-26 13:30:12 -0500

    Fixs spacings labels et columns. Fix affichage différents trucs.

--- extensions/snake/assets/custom.js
+++ extensions/snake/assets/custom.js
@@ -1,20 +1,35 @@
 (function ($) {
 	'use strict';
+
+	var win = $(window);
 	
 	var ready = function () {
 
-		/* Header - Nav */
+		/* ////////////////////////////////////////////////////////////////////////////
+		//
+		// Header
+		//
+		/////////////////////////////////////////////////////////////////////////// */
+
+		/* Nav - Init */
+
+		$('#nav li.active').has('ul').each(function(){
+			$(this).addClass('opened');
+			$('ul', this).show();
+		});
+
+		/* Nav - Toggle Submenu */
 
 		$('#nav li').has('ul').on('click', function(){
 			$(this).toggleClass('opened');
 			$(this).children('ul').slideToggle(250);
 		});
 
-		/* Header - Actions */
+		/* Actions - Init */
 
 		if($('.page-single #contents .actions, .single #contents .actions').length) $('#context').addClass('spaced-right');
 
-		/* Tabs */
+		/* Tabs - Init */
 
 		$('.tab-group:not(:first-child)').each(function(){
 			var t = $(this);
@@ -22,13 +37,50 @@
 			t.prepend('<h2>'+title+'</h2>');
 		});
 
+		/* Tabs - Anchors */
+
 		$('#context .tabs li').on('click', function(){
 			var t = $(this);
 
-			$('html, body').animate({scrollTop: ($('.tab-group').eq(t.index()).offset().top - 120)}, 750);
+			$('html, body').stop().animate({scrollTop: ($('.tab-group').eq(t.index()).offset().top - 120)}, 750);
 
 			return false;
 		});
+
+		/* ////////////////////////////////////////////////////////////////////////////
+		//
+		// onScroll
+		//
+		/////////////////////////////////////////////////////////////////////////// */
+
+		/* Scroll - Events */
+
+		onScroll();
+
+		win.on('scroll', function(){
+			onScroll();
+		});
+
+		/* Scroll - Function */
+
+		function onScroll(){
+			var pageEnd = $(document).height() - win.height();
+			var curScroll = win.scrollTop();
+
+			if(curScroll == pageEnd){
+				$('#context .tabs li').removeClass('selected');
+				$('#context .tabs li:last-child').addClass('selected');
+			} else {
+				$('.tab-group:not(:first-child)').each(function(){
+					var t = $(this);
+
+					if(curScroll + (win.height() / 2) > t.offset().top) {
+						$('#context .tabs li').removeClass('selected');
+						$('#context .tabs li').eq(t.index()).addClass('selected');
+					}
+				});
+			}
+		}
 	};
 	
 	$(ready);
