0c3e7418e52749132c49db1e35d0df11e5bd3526
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-01-26 09:57:27 -0500

    Version fonctionnelle des tabs avec le scroll. Début des .frames.

--- extensions/snake/assets/custom.js
+++ extensions/snake/assets/custom.js
@@ -13,6 +13,22 @@
 		/* Header - Actions */
 
 		if($('.page-single #contents .actions, .single #contents .actions').length) $('#context').addClass('spaced-right');
+
+		/* Tabs */
+
+		$('.tab-group:not(:first-child)').each(function(){
+			var t = $(this);
+			var title = $('#context .tabs li').eq(t.index()).html();
+			t.prepend('<h2>'+title+'</h2>');
+		});
+
+		$('#context .tabs li').on('click', function(){
+			var t = $(this);
+
+			$('html, body').animate({scrollTop: ($('.tab-group').eq(t.index()).offset().top - 120)}, 750);
+
+			return false;
+		});
 	};
 	
 	$(ready);
