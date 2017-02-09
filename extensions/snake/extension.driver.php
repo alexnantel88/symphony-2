5d55e03dad66fdb35b81b77321a3fdb868ca063c
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-02-09 15:17:57 -0500

    Intégration custom select2.js.

--- extensions/snake/extension.driver.php
+++ extensions/snake/extension.driver.php
@@ -20,8 +20,13 @@
 		 */
 		public function addScriptToHead($context)
 		{
+			// Custom
 			Administration::instance()->Page->addScriptToHead(URL.'/extensions/snake/assets/custom.js');
 			Administration::instance()->Page->addStylesheetToHead(URL.'/extensions/snake/assets/custom.css');
+
+			// CDN
+			Administration::instance()->Page->addScriptToHead('https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js');
+			Administration::instance()->Page->addStylesheetToHead('https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css');
 		}
 	}
 ?>
